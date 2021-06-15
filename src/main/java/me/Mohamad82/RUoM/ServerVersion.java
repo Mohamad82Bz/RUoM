e760b7b3f7adf24cb7bdef5df7adff6177b876db
Mohamad82Bz <mohamad.hossein82.bz@gmail.com>
2021-06-15 15:55:17 +0430

    v1.7.11 | New class added to check server's version with useful legacy checks

---
+++ src/main/java/me/Mohamad82/RUoM/ServerVersion.java
@@ -0,0 +1,20 @@
+package me.Mohamad82.RUoM;
+
+import org.bukkit.Bukkit;
+
+public class ServerVersion {
+
+    public static String getVersion() {
+        return Bukkit.getBukkitVersion().split("-")[0];
+    }
+
+    public static boolean is1_8() {
+        return getVersion().equals("1.8");
+    }
+
+    public static boolean isLegacy() {
+        String version = getVersion();
+        return version.equals("1.8") || version.equals("1.9") || version.equals("1.10") || version.equals("1.11") || version.equals("1.12");
+    }
+
+}
