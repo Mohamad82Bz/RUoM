50f4d404c10a7aedca8e5b027861289e78f1a346
Mohamad82Bz <mohamad.hossein82.bz@gmail.com>
2021-09-14 22:00:48 +0430

    v2.3.1 | Added new methods to PlayerUtils, Implemented BlockUtils

---
+++ src/main/java/me/Mohamad82/RUoM/utils/BlockUtils.java
@@ -0,0 +1,24 @@
+package me.Mohamad82.RUoM.utils;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Particle;
+
+import java.util.Random;
+
+public class BlockUtils {
+
+    private void spawnBlockBreakParticles(Location blockLocation, Material material) {
+        Location center = LocUtils.simplifyToCenter(blockLocation);
+        for (int i = 0; i <= 30; i++) {
+            blockLocation.getWorld().spawnParticle(Particle.BLOCK_CRACK,
+                    center.clone().add(getRandomInBlock(), getRandomInBlock() + 0.5, getRandomInBlock()),
+                    2, material.createBlockData());
+        }
+    }
+
+    private float getRandomInBlock() {
+        return (float) (new Random().nextInt(10) - 5) / 10;
+    }
+
+}
--- src/main/java/me/Mohamad82/RUoM/utils/PlayerUtils.java
+++ src/main/java/me/Mohamad82/RUoM/utils/PlayerUtils.java
@@ -2,9 +2,37 @@ package me.Mohamad82.RUoM.utils;
 
 import me.Mohamad82.RUoM.vector.Vector3;
 import me.Mohamad82.RUoM.vector.Vector3Utils;
+import org.bukkit.Location;
+import org.bukkit.Particle;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Random;
 
 public class PlayerUtils {
+
+    public static void spawnFoodEatParticles(Location location, ItemStack foodItem) {
+        final Random random = new Random();
+        final Location rightSide = getRightHandLocation(location).add(0, -0.25, 0);
+        for (int i = 0; i < 11; i++) {
+            if (random.nextInt(7) < 1) continue;
+            float a1 = (float) (random.nextInt(4) - 2) / 10;
+            float a2 = (float) (random.nextInt(4) - 2) / 10;
+            float a3 = (float) (random.nextInt(15) - 5) / 100;
+
+            location.getWorld().spawnParticle(Particle.ITEM_CRACK, rightSide,
+                    0, 0 + a1, 1, 0 + a2, 0.23 + a3,
+                    foodItem);
+        }
+    }
+
+    public static Location getRightHandLocation(Location location) {
+        double yawRightHandDirection = Math.toRadians(-1 * location.getYaw());
+        double x = 0.5 * Math.sin(yawRightHandDirection) + location.getX();
+        double y = location.getY() + 1;
+        double z = 0.5 * Math.cos(yawRightHandDirection) + location.getZ();
+        return new Location(location.getWorld(), x, y, z);
+    }
 
     public static Vector3 getPlayerVector3Location(Player player) {
         return Vector3Utils.toVector3(player.getLocation());
