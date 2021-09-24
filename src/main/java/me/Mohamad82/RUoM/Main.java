4095839389eec0ba7af755e6f48962b63cc4c369
Mohamad82Bz <mohamad.hossein82.bz@gmail.com>
2021-09-24 16:12:33 +0330

    v2.4 | Main class testing stuff

--- src/main/java/me/Mohamad82/RUoM/Main.java
+++ src/main/java/me/Mohamad82/RUoM/Main.java
@@ -1,15 +1,45 @@
 package me.Mohamad82.RUoM;
 
-public final class Main extends RUoMPlugin {
+import me.Mohamad82.RUoM.translators.skin.MineSkinAPI;
+import me.Mohamad82.RUoM.translators.skin.MinecraftSkin;
+import me.Mohamad82.RUoM.translators.skin.SkinBuilder;
+import me.Mohamad82.RUoM.utils.MilliCounter;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public final class Main extends RUoMPlugin implements CommandExecutor {
 
     @Override
     public void onEnable() {
-
+        getCommand("ruom").setExecutor(this);
+        new SkinBuilder();
     }
 
     @Override
     public void onDisable() {
+    }
 
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        MilliCounter counter = new MilliCounter();
+        counter.start();
+        try {
+            if (args[0].equalsIgnoreCase("mojang")) {
+                MinecraftSkin skin = SkinBuilder.getInstance().getSkin(args[1], true);
+                skin.apply((Player) sender);
+
+            } else if (args[0].equalsIgnoreCase("mineskin")) {
+                MinecraftSkin skin = SkinBuilder.getInstance().getSkin(args[1], MineSkinAPI.SkinType.NORMAL, true);
+                skin.apply((Player) sender);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        counter.stop();
+        sender.sendMessage("Took " + counter.get() + "ms");
+
+        return false;
     }
 
     /*public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
