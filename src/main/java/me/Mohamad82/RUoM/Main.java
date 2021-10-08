67333b9bba4b3cbf8c166ad0b21f70cfbf9125e0
Mohamad82Bz <mohamad.hossein82.bz@gmail.com>
2021-10-08 20:51:33 +0330

    v2.6 | Testing stuff

--- src/main/java/me/Mohamad82/RUoM/Main.java
+++ src/main/java/me/Mohamad82/RUoM/Main.java
@@ -1,9 +1,17 @@
 package me.Mohamad82.RUoM;
 
+import com.cryptomorin.xseries.XMaterial;
+import com.google.gson.GsonBuilder;
+import me.Mohamad82.RUoM.adventureapi.AdventureAPIManager;
+import me.Mohamad82.RUoM.adventureapi.ComponentUtils;
 import me.Mohamad82.RUoM.translators.skin.MineSkinAPI;
 import me.Mohamad82.RUoM.translators.skin.MinecraftSkin;
 import me.Mohamad82.RUoM.translators.skin.SkinBuilder;
 import me.Mohamad82.RUoM.utils.MilliCounter;
+import me.Mohamad82.RUoM.utils.ToastMessage;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,6 +23,7 @@ public final class Main extends RUoMPlugin implements CommandExecutor {
     public void onEnable() {
         getCommand("ruom").setExecutor(this);
         new SkinBuilder();
+        Ruom.initializeAdventure();
     }
 
     @Override
@@ -25,7 +34,18 @@ public final class Main extends RUoMPlugin implements CommandExecutor {
         MilliCounter counter = new MilliCounter();
         counter.start();
         try {
-            if (args[0].equalsIgnoreCase("mojang")) {
+            if (args[0].equalsIgnoreCase("toast")) {
+                ToastMessage toastMessage = ToastMessage.create(args[1], XMaterial.valueOf(args[2].toUpperCase()), ToastMessage.FrameType.valueOf(args[3].toUpperCase()), Boolean.parseBoolean(args[4]));
+                toastMessage.send((Player) sender);
+            } else if (args[0].equalsIgnoreCase("esc")) {
+                Component component = ComponentUtils.parseD(args[1]);
+                AdventureAPIManager.getAdventure().player((Player) sender).sendMessage(component);
+                Bukkit.broadcastMessage(new GsonBuilder().setPrettyPrinting().create().toJson(GsonComponentSerializer.gson().serialize(component)));
+            } else if (args[0].equalsIgnoreCase("esc2")) {
+                Component component = ComponentUtils.parse(args[1]);
+                AdventureAPIManager.getAdventure().player((Player) sender).sendMessage(component);
+                Bukkit.broadcastMessage(new GsonBuilder().setPrettyPrinting().create().toJson(GsonComponentSerializer.gson().serialize(component)));
+            } else if (args[0].equalsIgnoreCase("mojang")) {
                 MinecraftSkin skin = SkinBuilder.getInstance().getSkin(args[1], true);
                 skin.apply((Player) sender);
 
