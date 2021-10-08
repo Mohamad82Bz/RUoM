b6bd305bc5085e87d824be7c9b893f2307c8eab4
Mohamad82Bz <mohamad.hossein82.bz@gmail.com>
2021-10-09 00:05:42 +0330

    v2.6.3 | Now parsing legacy color codes in components

--- src/main/java/me/Mohamad82/RUoM/adventureapi/ComponentUtils.java
+++ src/main/java/me/Mohamad82/RUoM/adventureapi/ComponentUtils.java
@@ -16,7 +16,32 @@ public class ComponentUtils {
     }
 
     public static Component parseD(String string) {
-        return MiniMessage.withMarkdownFlavor(DiscordFlavor.get()).parse(string);
+        return MiniMessage.withMarkdownFlavor(DiscordFlavor.get()).parse(parseComponentColors(string));
+    }
+
+    public static String parseComponentColors(String msg) {
+        return msg.replace("&0", "<reset><black>")
+                .replace("&1", "<reset><dark_blue>")
+                .replace("&2", "<reset><dark_green>")
+                .replace("&3", "<reset>dark_blue")
+                .replace("&4", "<reset><dark_red>")
+                .replace("&5", "<reset><dark_purple>")
+                .replace("&6", "<reset><gold>")
+                .replace("&7", "<reset><gray>")
+                .replace("&8", "<reset><dark_gray>")
+                .replace("&9", "<reset><blue>")
+                .replace("&a", "<reset><green>")
+                .replace("&b", "<reset><aqua>")
+                .replace("&c", "<reset><red>")
+                .replace("&d", "<reset><light_purple>")
+                .replace("&e", "<reset><yellow>")
+                .replace("&f", "<reset><white>")
+                .replace("&l", "<bold>")
+                .replace("&n", "<underlined>")
+                .replace("&m", "<strikethrough>")
+                .replace("&o", "<italic>")
+                .replace("&r", "<reset>")
+                .replace("&k", "<obfuscated>");
     }
 
 }
