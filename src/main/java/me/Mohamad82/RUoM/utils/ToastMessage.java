d171f8a368ddc2ed694404589ca72da4f40885f6
Mohamad82Bz <mohamad.hossein82.bz@gmail.com>
2021-10-20 01:06:27 +0330

    v2.7 | ToastMessage is now thread-safe and packet-based

--- src/main/java/me/Mohamad82/RUoM/utils/ToastMessage.java
+++ src/main/java/me/Mohamad82/RUoM/utils/ToastMessage.java
@@ -2,131 +2,214 @@ package me.Mohamad82.RUoM.utils;
 
 import com.cryptomorin.xseries.ReflectionUtils;
 import com.cryptomorin.xseries.XMaterial;
-import com.google.gson.*;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import me.Mohamad82.RUoM.Ruom;
 import me.Mohamad82.RUoM.adventureapi.AdventureAPIManager;
 import me.Mohamad82.RUoM.adventureapi.ComponentUtils;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
-import org.bukkit.Bukkit;
-import org.bukkit.NamespacedKey;
-import org.bukkit.UnsafeValues;
-import org.bukkit.World;
-import org.bukkit.advancement.Advancement;
 import org.bukkit.entity.Player;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.UUID;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.*;
 import java.util.regex.Matcher;
 
-@SuppressWarnings("deprecation")
 public class ToastMessage {
 
-    private final static char ignoreChar = 'ˑ';
+    private final char ignoreChar = 'ˑ';
+    private final JsonParser jsonParser = new JsonParser();
 
-    private String title;
-    private final NamespacedKey id;
-    private final Advancement advancement;
+    private Object packet;
+    private Object removePacket;
+    private Object advancementProgress;
+
+    private static Class<?> MINECRAFT_KEY, ADVANCEMENT, SERIALIZED_ADVANCEMENT, LOOT_DESERIALIZATION_CONTEXT, LOOT_PREDICATE_MANAGER, ADVANCEMENT_PROGRESS,
+    PACKET_PLAY_OUT_ADVANCEMENTS, CRAFT_ADVANCEMENT_PROGRESS;
+
+    private static Constructor<?> MINECRAFT_KEY_CONSTRUCTOR, LOOT_DESERIALIZATION_CONTEXT_CONSTRUCTOR, LOOT_PREDICATE_MANAGER_CONSTRUCTOR, ADVANCEMENT_PROGRESS_CONSTRUCTOR,
+    PACKET_PLAY_OUT_ADVANCEMENTS_CONSTRUCTOR;
+
+    private static Method ADVANCEMENT_SERIALIZE_METHOD, ADVANCEMENT_DESERIALIZE_METHOD, ADVANCEMENT_PROGRESS_UPDATE_METHOD,
+    ADVANCEMENT_PROGRESS_GRANT_CRITERIA_METHOD, ADVANCEMENT_PROGRESS_REVOKE_CRITERIA_METHOD, ADVANCEMENT_GET_CRITERIA_METHOD, ADVANCEMENT_GET_REQUIREMENTS_METHOD;
+
+    private static Field CRAFT_ADVANCEMENT_PROGRESS_HANDLE_FIELD;
+
+    static {
+        try {
+            {
+                MINECRAFT_KEY = ReflectionUtils.getNMSClass("resources", "MinecraftKey");
+                ADVANCEMENT = ReflectionUtils.getNMSClass("advancements", "Advancement");
+                SERIALIZED_ADVANCEMENT = ReflectionUtils.getNMSClass("advancements", "Advancement$SerializedAdvancement");
+                if (ServerVersion.supports(13)) {
+                    LOOT_DESERIALIZATION_CONTEXT = ReflectionUtils.getNMSClass("advancements.critereon", "LootDeserializationContext");
+                    LOOT_PREDICATE_MANAGER = ReflectionUtils.getNMSClass("world.level.storage.loot", "LootPredicateManager");
+                }
+                ADVANCEMENT_PROGRESS = ReflectionUtils.getNMSClass("advancements", "AdvancementProgress");
+                PACKET_PLAY_OUT_ADVANCEMENTS = ReflectionUtils.getNMSClass("network.protocol.game", "PacketPlayOutAdvancements");
+                CRAFT_ADVANCEMENT_PROGRESS = ReflectionUtils.getCraftClass("advancement.CraftAdvancementProgress");
+            }
+            {
+                MINECRAFT_KEY_CONSTRUCTOR = MINECRAFT_KEY.getConstructor(String.class);
+                if (ServerVersion.supports(13)) {
+                    LOOT_DESERIALIZATION_CONTEXT_CONSTRUCTOR = LOOT_DESERIALIZATION_CONTEXT.getConstructor(MINECRAFT_KEY, LOOT_PREDICATE_MANAGER);
+                    LOOT_PREDICATE_MANAGER_CONSTRUCTOR = LOOT_PREDICATE_MANAGER.getConstructor();
+                }
+                ADVANCEMENT_PROGRESS_CONSTRUCTOR = ADVANCEMENT_PROGRESS.getConstructor();
+                PACKET_PLAY_OUT_ADVANCEMENTS_CONSTRUCTOR = PACKET_PLAY_OUT_ADVANCEMENTS.getConstructor(boolean.class, Collection.class, Set.class, Map.class);
+            }
+            {
+                if (ServerVersion.supports(13)) {
+                    ADVANCEMENT_SERIALIZE_METHOD = SERIALIZED_ADVANCEMENT.getMethod("a", JsonObject.class, LOOT_DESERIALIZATION_CONTEXT);
+                }
+                ADVANCEMENT_DESERIALIZE_METHOD = SERIALIZED_ADVANCEMENT.getMethod(ServerVersion.supports(13) ? "b" : "a", MINECRAFT_KEY);
+                ADVANCEMENT_PROGRESS_UPDATE_METHOD = ADVANCEMENT_PROGRESS.getMethod("a", Map.class, String[][].class);
+                ADVANCEMENT_PROGRESS_GRANT_CRITERIA_METHOD = ADVANCEMENT_PROGRESS.getMethod("a", String.class);
+                ADVANCEMENT_PROGRESS_REVOKE_CRITERIA_METHOD = ADVANCEMENT_PROGRESS.getMethod("b", String.class);
+                ADVANCEMENT_GET_CRITERIA_METHOD = ADVANCEMENT.getMethod("getCriteria");
+                ADVANCEMENT_GET_REQUIREMENTS_METHOD = ADVANCEMENT.getMethod("i");
+            }
+            {
+                CRAFT_ADVANCEMENT_PROGRESS_HANDLE_FIELD = CRAFT_ADVANCEMENT_PROGRESS.getDeclaredField("handle");
+                CRAFT_ADVANCEMENT_PROGRESS_HANDLE_FIELD.setAccessible(true);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ToastMessage create(String title, XMaterial icon, FrameType frameType, boolean trimCharacters) {
+        return new ToastMessage(title, icon, frameType, trimCharacters);
+    }
 
     protected ToastMessage(String title, XMaterial icon, FrameType frameType, boolean trimCharacters) {
-        this.title = title;
-        id = new NamespacedKey(Ruom.getPlugin(), "ruom/toasts/" + UUID.randomUUID());
+        JsonObject jsonAdvancement = new JsonObject();
 
-        String iconId = "minecraft:" + icon.parseMaterial().toString().toLowerCase();
+        JsonObject displayJson = new JsonObject();
+        JsonObject iconJson = new JsonObject();
+        JsonObject descJson = new JsonObject();
 
-        JsonObject json = new JsonObject();
-        JsonObject iconObject = new JsonObject();
+        JsonObject criteriaJson = new JsonObject();
+        JsonObject elytraJson = new JsonObject();
 
-        iconObject.addProperty("item", iconId);
-        if (!ServerVersion.supports(13)) {
-            iconObject.addProperty("data", icon.getData());
-        }
+        JsonArray requirementsArray = new JsonArray();
+        JsonArray elytraRequirementArray = new JsonArray();
 
-        JsonObject displayObject = new JsonObject();
-        displayObject.add("icon", iconObject);
+        iconJson.addProperty("item", "minecraft:" + icon.parseMaterial().toString().toLowerCase());
+        if (!ServerVersion.supports(13))
+            iconJson.addProperty("data", icon.getData());
+        descJson.addProperty("text", "");
+        displayJson.add("title", parseTitle(title, trimCharacters));
+        displayJson.add("icon", iconJson);
+        displayJson.add("description", descJson);
+        displayJson.addProperty("frame", frameType.toString().toLowerCase());
+        displayJson.addProperty("show_toast", true);
+        displayJson.addProperty("announce_to_chat", false);
+        displayJson.addProperty("hidden", false);
+
+        elytraJson.addProperty("trigger", "minecraft:impossible");
+        criteriaJson.add("elytra", elytraJson);
+
+        elytraRequirementArray.add("elytra");
+        requirementsArray.add(elytraRequirementArray);
+
+        jsonAdvancement.add("display", displayJson);
+        jsonAdvancement.add("criteria", criteriaJson);
+        jsonAdvancement.add("requirements", requirementsArray);
 
         try {
-            Class.forName("net.kyori.adventure.text.Component");
+            Object advancementKey = MINECRAFT_KEY_CONSTRUCTOR.newInstance("ruom_toasts" + UUID.randomUUID());
+            Object nmsSerializedAdvancement = ADVANCEMENT_SERIALIZE_METHOD.invoke(null, jsonAdvancement, LOOT_DESERIALIZATION_CONTEXT_CONSTRUCTOR.newInstance(advancementKey, LOOT_PREDICATE_MANAGER_CONSTRUCTOR.newInstance()));
+            Object nmsAdvancement = ADVANCEMENT_DESERIALIZE_METHOD.invoke(nmsSerializedAdvancement, advancementKey);
+            Object criteriaObject = ADVANCEMENT_GET_CRITERIA_METHOD.invoke(nmsAdvancement);
+            Object requirementObject = ADVANCEMENT_GET_REQUIREMENTS_METHOD.invoke(nmsAdvancement);
+            this.advancementProgress = ADVANCEMENT_PROGRESS_CONSTRUCTOR.newInstance();
 
-            if (trimCharacters) {
-                title = title.replace(String.valueOf(ignoreChar), ".");
-                String escapedTitle = replaceTokensWithIgnoreChar(title);
-                int i = 0;
-                int j = 0;
-                for (char character : escapedTitle.toCharArray()) {
-                    j++;
-                    if (character != ignoreChar)
-                        i++;
-                    if (i > 40)
-                        break;
-                }
-                title = title.substring(0, j);
-                if (i > 40)
-                    title = title + "...";
-            }
+            ADVANCEMENT_PROGRESS_UPDATE_METHOD.invoke(advancementProgress, criteriaObject, requirementObject);
 
-            Component component = ComponentUtils.parseD(title);
-            displayObject.add("title", parseJsonString(GsonComponentSerializer.gson().serialize(component)));
-        } catch (ClassNotFoundException e2) {
-            trimTitleCharacters();
-            displayObject.addProperty("title", title);
+            Collection<Object> toAddSet = new HashSet<>();
+            toAddSet.add(nmsAdvancement);
+            Map<Object, Object> advancementMap = new HashMap<>();
+            advancementMap.put(advancementKey, advancementProgress);
+            Set<Object> toRemoveSet = new HashSet<>();
+            toRemoveSet.add(advancementKey);
+
+            packet = PACKET_PLAY_OUT_ADVANCEMENTS_CONSTRUCTOR.newInstance(false, toAddSet, Collections.EMPTY_SET, advancementMap);
+            removePacket = PACKET_PLAY_OUT_ADVANCEMENTS_CONSTRUCTOR.newInstance(false, Collections.EMPTY_SET, toRemoveSet, Collections.EMPTY_MAP);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        displayObject.addProperty("description", "");
-        displayObject.addProperty("frame", frameType.toString().toLowerCase());
-        displayObject.addProperty("announce_to_chat", false);
-        displayObject.addProperty("show_toast", true);
-        displayObject.addProperty("hidden", true);
+    }
 
-        JsonObject criteriaObject = new JsonObject();
-        JsonObject conditionsObject = new JsonObject();
-        JsonObject elytra = new JsonObject();
-        JsonArray itemArray = new JsonArray();
+    public void send(Player... players) {
+        awardCriteria();
+        for (Player player : players) {
+            ReflectionUtils.sendPacket(player, packet);
+        }
 
-        conditionsObject.add("items", itemArray);
-        elytra.addProperty("trigger", "minecraft:impossible");
-        elytra.add("conditions", conditionsObject);
-        criteriaObject.add("elytra", elytra);
-
-        json.add("criteria", criteriaObject);
-        json.add("display", displayObject);
-
-        String jsonString = new GsonBuilder().setPrettyPrinting().create().toJson(json);
-
-        for (World world : Bukkit.getWorlds()) {
-            Path path = Paths.get(world.getWorldFolder() + File.separator + "data"
-                    + File.separator + "advancements" + File.separator + id.getNamespace() + File.separator + id.getKey().split("/")[0]);
-            Path path2 = Paths.get(world.getWorldFolder() + File.separator + "data"
-                    + File.separator + "advancements" + File.separator + id.getNamespace()
-                    + File.separator + id.getKey().split("/")[0] + File.separator + id.getKey().split("/")[1] + ".json");
-
-            if (!path.toFile().exists()){
-                path.toFile().mkdirs();
-            }
-
-            if (!path2.toFile().exists()){
-                File file = path2.toFile();
+        Ruom.runSync(() -> {
+            revokeCriteria();
+            Arrays.stream(players).forEach(player -> {
                 try {
-                    file.createNewFile();
-                    FileWriter writer = new FileWriter(file);
-                    writer.write(jsonString);
-                    writer.flush();
-                    writer.close();
-                } catch (IOException e) {
+                    ReflectionUtils.sendPacket(player, removePacket);
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
-            }
-        }
+            });
+        }, 2);
+    }
 
-        Bukkit.getUnsafe().loadAdvancement(id, jsonString);
-        this.advancement = Bukkit.getAdvancement(id);
+    private void awardCriteria() {
+        try {
+            ADVANCEMENT_PROGRESS_GRANT_CRITERIA_METHOD.invoke(advancementProgress, "elytra");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void revokeCriteria() {
+        try {
+            ADVANCEMENT_PROGRESS_REVOKE_CRITERIA_METHOD.invoke(advancementProgress, "elytra");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private JsonElement parseTitle(String rawTitle, boolean trimCharacters) {
+        if (trimCharacters)
+            rawTitle = trimCharacters(rawTitle);
+
+        Component component = ComponentUtils.parse(rawTitle);
+        return jsonParser.parse(GsonComponentSerializer.gson().serialize(component));
+    }
+
+    private String trimCharacters(String input) {
+        final int characterLimit = 45;
+
+        input = input.replace(String.valueOf(ignoreChar), ".");
+        String modifiedInput = replaceTokensWithIgnoreChar(input);
+        int i = 0;
+        int j = 0;
+        for (char character : modifiedInput.toCharArray()) {
+            j++;
+            if (character != ignoreChar)
+                i++;
+            if (i > characterLimit)
+                break;
+        }
+        input = input.substring(0, j);
+        if (i > characterLimit)
+            input = input + "...";
+
+        return input;
     }
 
     @SuppressWarnings("SuspiciousRegexArgument")
-    private static String replaceTokensWithIgnoreChar(final String richMessage) {
+    private String replaceTokensWithIgnoreChar(final String richMessage) {
         final StringBuilder sb = new StringBuilder();
         final Matcher matcher = AdventureAPIManager.escapeTokenPattern.matcher(richMessage);
         int lastEnd = 0;
@@ -155,66 +238,6 @@ public class ToastMessage {
         }
 
         return sb.toString();
-    }
-
-    /**
-     * Creates a ToastMessage that can be sent to players.
-     * @param title Title of the message
-     * @param icon Icon of the message
-     * @param frameType Frame type of the message
-     * @param trimCharacters Total of 2 lines can fit in the toast messages' frames which is ~40 characters, Set this to true if you want to limit the characters
-     * @return A ready-to-send toast message
-     */
-    public static ToastMessage create(String title, XMaterial icon, FrameType frameType, boolean trimCharacters) {
-        return new ToastMessage(title, icon, frameType, trimCharacters);
-    }
-
-    public static ToastMessage create(String title, XMaterial icon, FrameType frameType) {
-        return new ToastMessage(title, icon, frameType, true);
-    }
-
-    public void send(Player... players) {
-        for (Player player : players) {
-            if (!player.getAdvancementProgress(advancement).isDone()) {
-                player.getAdvancementProgress(advancement).awardCriteria("elytra");
-                Bukkit.getScheduler().runTaskLater(Ruom.getPlugin(), new Runnable() {
-                    @Override
-                    public void run() {
-                        player.getAdvancementProgress(advancement).revokeCriteria("elytra");
-                    }
-                }, 5);
-            }
-        }
-    }
-
-    public void sendAndDelete(Player... players) {
-        send(players);
-        delete();
-    }
-
-    public void delete() {
-        Bukkit.getScheduler().runTaskLater(Ruom.getPlugin(), new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Class<?> CRAFT_MAGIC_NUMBERS = ReflectionUtils.getCraftClass("util.CraftMagicNumbers");
-                    UnsafeValues unsafeValues = (UnsafeValues) CRAFT_MAGIC_NUMBERS.getField("INSTANCE").get(null);
-                    unsafeValues.removeAdvancement(id);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }, 5);
-    }
-
-    private JsonElement parseJsonString(String jsonString) {
-        return new JsonParser().parse(jsonString);
-    }
-
-    private void trimTitleCharacters() {
-        if (title.length() > 40) {
-            title = title.substring(0, 40);
-        }
     }
 
     public enum FrameType {
