e905f2c83ff07ea220c558fb676dbc9c32c34029
Mohamad82Bz <mohamad.hossein82.bz@gmail.com>
2021-10-20 01:04:27 +0330

    v2.7 | Added scheduler shortcuts and some other new methods

--- src/main/java/me/Mohamad82/RUoM/Ruom.java
+++ src/main/java/me/Mohamad82/RUoM/Ruom.java
@@ -6,7 +6,16 @@ import me.Mohamad82.RUoM.areaselection.AreaSelectionManager;
 import me.Mohamad82.RUoM.gui.GUIListener;
 import me.Mohamad82.RUoM.worldedit.WEManager;
 import me.Mohamad82.RUoM.worldedit.enums.WEType;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Ruom {
 
@@ -16,12 +25,24 @@ public class Ruom {
         return RUoMPlugin.get();
     }
 
+    public static Server getServer() {
+        return getPlugin().getServer();
+    }
+
+    public static Set<Player> getOnlinePlayers() {
+        return new HashSet<>(getServer().getOnlinePlayers());
+    }
+
     public static void setDebug(boolean debug) {
         Ruom.debug = debug;
     }
 
     public static void registerListener(Listener listener) {
         RUoMPlugin.get().getServer().getPluginManager().registerEvents(listener, RUoMPlugin.get());
+    }
+
+    public static void unregisterListener(Listener listener) {
+        HandlerList.unregisterAll(listener);
     }
 
     public static void initializeAdventure() {
@@ -41,6 +62,14 @@ public class Ruom {
         new WEManager(RUoMPlugin.get(), type);
     }
 
+    public static void broadcast(String message) {
+        Bukkit.broadcastMessage(message);
+    }
+
+    public static void broadcast(Component message) {
+        AdventureAPIManager.getAdventure().players().sendMessage(message);
+    }
+
     public static void log(String message) {
         RUoMPlugin.get().getLogger().info(message);
     }
@@ -57,6 +86,30 @@ public class Ruom {
 
     public static void error(String message) {
         RUoMPlugin.get().getLogger().severe(message);
+    }
+
+    public static BukkitTask runSync(Runnable runnable) {
+        return Bukkit.getScheduler().runTask(getPlugin(), runnable);
+    }
+
+    public static BukkitTask runSync(Runnable runnable, int delay) {
+        return Bukkit.getScheduler().runTaskLater(getPlugin(), runnable, delay);
+    }
+
+    public static BukkitTask runSync(Runnable runnable, int delay, int period) {
+        return Bukkit.getScheduler().runTaskTimer(getPlugin(), runnable, delay, period);
+    }
+
+    public static BukkitTask runAsync(Runnable runnable) {
+        return Bukkit.getScheduler().runTaskAsynchronously(getPlugin(), runnable);
+    }
+
+    public static BukkitTask runAsync(Runnable runnable, int delay) {
+        return Bukkit.getScheduler().runTaskLaterAsynchronously(getPlugin(), runnable, delay);
+    }
+
+    public static BukkitTask runAsync(Runnable runnable, int delay, int period) {
+        return Bukkit.getScheduler().runTaskTimerAsynchronously(getPlugin(), runnable, delay, period);
     }
 
     public static void shutdown() {
