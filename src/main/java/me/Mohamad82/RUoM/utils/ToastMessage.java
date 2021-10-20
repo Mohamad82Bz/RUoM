f3608c45844ed5e855dd766c0a1d95ec428db190
Mohamad82Bz <mohamad.hossein82.bz@gmail.com>
2021-10-20 18:43:44 +0330

    v2.7.1 | Added 1.12 compatibility for new ToastMessage

--- src/main/java/me/Mohamad82/RUoM/utils/ToastMessage.java
+++ src/main/java/me/Mohamad82/RUoM/utils/ToastMessage.java
@@ -2,10 +2,7 @@ package me.Mohamad82.RUoM.utils;
 
 import com.cryptomorin.xseries.ReflectionUtils;
 import com.cryptomorin.xseries.XMaterial;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 import me.Mohamad82.RUoM.Ruom;
 import me.Mohamad82.RUoM.adventureapi.AdventureAPIManager;
 import me.Mohamad82.RUoM.adventureapi.ComponentUtils;
@@ -22,19 +19,22 @@ import java.util.regex.Matcher;
 public class ToastMessage {
 
     private final char ignoreChar = 'ˑ';
+    private final Gson gson = new GsonBuilder().create();
     private final JsonParser jsonParser = new JsonParser();
 
     private Object packet;
     private Object removePacket;
     private Object advancementProgress;
 
+    private static Object DESERIALIZER;
+
     private static Class<?> MINECRAFT_KEY, ADVANCEMENT, SERIALIZED_ADVANCEMENT, LOOT_DESERIALIZATION_CONTEXT, LOOT_PREDICATE_MANAGER, ADVANCEMENT_PROGRESS,
-    PACKET_PLAY_OUT_ADVANCEMENTS, CRAFT_ADVANCEMENT_PROGRESS;
+    PACKET_PLAY_OUT_ADVANCEMENTS, CRAFT_ADVANCEMENT_PROGRESS, CHAT_DESERIALIZER;
 
     private static Constructor<?> MINECRAFT_KEY_CONSTRUCTOR, LOOT_DESERIALIZATION_CONTEXT_CONSTRUCTOR, LOOT_PREDICATE_MANAGER_CONSTRUCTOR, ADVANCEMENT_PROGRESS_CONSTRUCTOR,
     PACKET_PLAY_OUT_ADVANCEMENTS_CONSTRUCTOR;
 
-    private static Method ADVANCEMENT_SERIALIZE_METHOD, ADVANCEMENT_DESERIALIZE_METHOD, ADVANCEMENT_PROGRESS_UPDATE_METHOD,
+    private static Method ADVANCEMENT_SERIALIZE_METHOD, ADVANCEMENT_DESERIALIZE_METHOD, ADVANCEMENT_PROGRESS_UPDATE_METHOD, CHAT_DESERIALIZER_METHOD,
     ADVANCEMENT_PROGRESS_GRANT_CRITERIA_METHOD, ADVANCEMENT_PROGRESS_REVOKE_CRITERIA_METHOD, ADVANCEMENT_GET_CRITERIA_METHOD, ADVANCEMENT_GET_REQUIREMENTS_METHOD;
 
     private static Field CRAFT_ADVANCEMENT_PROGRESS_HANDLE_FIELD;
@@ -42,29 +42,36 @@ public class ToastMessage {
     static {
         try {
             {
+                DESERIALIZER = ReflectionUtils.getNMSClass("AdvancementDataWorld").getField("DESERIALIZER").get(null);
+            }
+            {
                 MINECRAFT_KEY = ReflectionUtils.getNMSClass("resources", "MinecraftKey");
                 ADVANCEMENT = ReflectionUtils.getNMSClass("advancements", "Advancement");
                 SERIALIZED_ADVANCEMENT = ReflectionUtils.getNMSClass("advancements", "Advancement$SerializedAdvancement");
-                if (ServerVersion.supports(13)) {
-                    LOOT_DESERIALIZATION_CONTEXT = ReflectionUtils.getNMSClass("advancements.critereon", "LootDeserializationContext");
-                    LOOT_PREDICATE_MANAGER = ReflectionUtils.getNMSClass("world.level.storage.loot", "LootPredicateManager");
-                }
                 ADVANCEMENT_PROGRESS = ReflectionUtils.getNMSClass("advancements", "AdvancementProgress");
                 PACKET_PLAY_OUT_ADVANCEMENTS = ReflectionUtils.getNMSClass("network.protocol.game", "PacketPlayOutAdvancements");
                 CRAFT_ADVANCEMENT_PROGRESS = ReflectionUtils.getCraftClass("advancement.CraftAdvancementProgress");
+                if (ServerVersion.supports(13)) {
+                    LOOT_DESERIALIZATION_CONTEXT = ReflectionUtils.getNMSClass("advancements.critereon", "LootDeserializationContext");
+                    LOOT_PREDICATE_MANAGER = ReflectionUtils.getNMSClass("world.level.storage.loot", "LootPredicateManager");
+                } else {
+                    CHAT_DESERIALIZER = ReflectionUtils.getNMSClass("ChatDeserializer");
+                }
             }
             {
                 MINECRAFT_KEY_CONSTRUCTOR = MINECRAFT_KEY.getConstructor(String.class);
+                ADVANCEMENT_PROGRESS_CONSTRUCTOR = ADVANCEMENT_PROGRESS.getConstructor();
+                PACKET_PLAY_OUT_ADVANCEMENTS_CONSTRUCTOR = PACKET_PLAY_OUT_ADVANCEMENTS.getConstructor(boolean.class, Collection.class, Set.class, Map.class);
                 if (ServerVersion.supports(13)) {
                     LOOT_DESERIALIZATION_CONTEXT_CONSTRUCTOR = LOOT_DESERIALIZATION_CONTEXT.getConstructor(MINECRAFT_KEY, LOOT_PREDICATE_MANAGER);
                     LOOT_PREDICATE_MANAGER_CONSTRUCTOR = LOOT_PREDICATE_MANAGER.getConstructor();
                 }
-                ADVANCEMENT_PROGRESS_CONSTRUCTOR = ADVANCEMENT_PROGRESS.getConstructor();
-                PACKET_PLAY_OUT_ADVANCEMENTS_CONSTRUCTOR = PACKET_PLAY_OUT_ADVANCEMENTS.getConstructor(boolean.class, Collection.class, Set.class, Map.class);
             }
             {
                 if (ServerVersion.supports(13)) {
                     ADVANCEMENT_SERIALIZE_METHOD = SERIALIZED_ADVANCEMENT.getMethod("a", JsonObject.class, LOOT_DESERIALIZATION_CONTEXT);
+                } else {
+                    CHAT_DESERIALIZER_METHOD = CHAT_DESERIALIZER.getMethod("a", DESERIALIZER.getClass(), String.class, Class.class);
                 }
                 ADVANCEMENT_DESERIALIZE_METHOD = SERIALIZED_ADVANCEMENT.getMethod(ServerVersion.supports(13) ? "b" : "a", MINECRAFT_KEY);
                 ADVANCEMENT_PROGRESS_UPDATE_METHOD = ADVANCEMENT_PROGRESS.getMethod("a", Map.class, String[][].class);
@@ -123,7 +130,12 @@ public class ToastMessage {
 
         try {
             Object advancementKey = MINECRAFT_KEY_CONSTRUCTOR.newInstance("ruom_toasts" + UUID.randomUUID());
-            Object nmsSerializedAdvancement = ADVANCEMENT_SERIALIZE_METHOD.invoke(null, jsonAdvancement, LOOT_DESERIALIZATION_CONTEXT_CONSTRUCTOR.newInstance(advancementKey, LOOT_PREDICATE_MANAGER_CONSTRUCTOR.newInstance()));
+            Object nmsSerializedAdvancement;
+            if (ServerVersion.supports(13)) {
+                nmsSerializedAdvancement = ADVANCEMENT_SERIALIZE_METHOD.invoke(null, jsonAdvancement, LOOT_DESERIALIZATION_CONTEXT_CONSTRUCTOR.newInstance(advancementKey, LOOT_PREDICATE_MANAGER_CONSTRUCTOR.newInstance()));
+            } else {
+                nmsSerializedAdvancement = CHAT_DESERIALIZER_METHOD.invoke(null, DESERIALIZER, gson.toJson(jsonAdvancement), SERIALIZED_ADVANCEMENT);
+            }
             Object nmsAdvancement = ADVANCEMENT_DESERIALIZE_METHOD.invoke(nmsSerializedAdvancement, advancementKey);
             Object criteriaObject = ADVANCEMENT_GET_CRITERIA_METHOD.invoke(nmsAdvancement);
             Object requirementObject = ADVANCEMENT_GET_REQUIREMENTS_METHOD.invoke(nmsAdvancement);
