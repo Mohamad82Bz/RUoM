0c0600db9de64f7a753838822ffa383320e6f2d9
Mohamad82Bz <mohamad.hossein82.bz@gmail.com>
2023-01-05 02:10:34 +0330

    Added location-related kotlin extension methods

--- bukkit/src/main/kotlin/me/mohamad82/ruom/kotlinextensions/LocationExtension.kt
+++ bukkit/src/main/kotlin/me/mohamad82/ruom/kotlinextensions/LocationExtension.kt
@@ -1,8 +1,14 @@
 package me.mohamad82.ruom.kotlinextensions
 
+import me.mohamad82.ruom.math.vector.Vector3
+import me.mohamad82.ruom.math.vector.Vector3UtilsBukkit
 import me.mohamad82.ruom.utils.LocUtils
 import org.bukkit.Location
 
 fun Location.getAsString(): String {
     return LocUtils.toString(this)
+}
+
+fun Location.toVector3(): Vector3 {
+    return Vector3UtilsBukkit.toVector3(this)
 }
